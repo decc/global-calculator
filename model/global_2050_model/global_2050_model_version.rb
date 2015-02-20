e6ea5ac9199e46ff7468634d7bfd40946c4a58be
Tom Counsell <tom@counsell.org>
2015-02-20 07:19:42 +0000

    Updated the model to version 3.99.0

--- model/global_2050_model/global_2050_model_version.rb
+++ model/global_2050_model/global_2050_model_version.rb
@@ -1,1 +1,1 @@
-def Global2050Model.last_modified_date() @last_modified_date ||= Time.utc(*[30, 19, 13, 26, 1, 2015, 1, 26, false, "GMT"]); end
+def Global2050Model.last_modified_date() @last_modified_date ||= Time.utc(*[36, 59, 0, 20, 2, 2015, 5, 51, false, "GMT"]); end
